1a424e5e87962287f8c8ee8bc4c0727fa1b17d2c
lacklux <olayiwolaa35@gmail.com>
2025-08-20 13:45:47 +0100

    add templates and app view

---
+++ backend/app.py
@@ -0,0 +1,27 @@
+from flask import Flask, render_template,request
+import os
+
+frontend_path = os.path.join(os.path.dirname(__file__), '..', 'frontend')
+
+app = Flask(
+    __name__,
+    template_folder=os.path.join(frontend_path, 'templates'),
+    static_folder=os.path.join(frontend_path, 'static')
+)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+
+
+@app.route('/quadratic', methods=['GET', 'POST'])
+def quadratic():
+    if request.method == 'POST':
+        pass
+    return render_template('quadratic.html')
+
+
+if __name__ == '__main__':
+            app.run(debug=True)
+
